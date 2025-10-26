90b236a6fb775b2688379e0e7121ccbaef4fe923
killaketa <ryancartagena@gmail.com>
2025-10-26 00:43:26 -0400

    Added Winner/Loser Sounds Options

--- EndRoundSounds/EndRoundSoundsPlugin.cs
+++ EndRoundSounds/EndRoundSoundsPlugin.cs
@@ -1,6 +1,9 @@
 ﻿using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes;
+using CounterStrikeSharp.API.Modules.Entities;
+using CounterStrikeSharp.API.Modules.Utils;
+using Microsoft.Extensions.Logging;
 
 namespace EndRoundSounds;
 
@@ -23,7 +26,15 @@ public class EndRoundSoundsPlugin : BasePlugin, IPluginConfig<EndRoundSoundsConf
     public void OnConfigParsed(EndRoundSoundsConfig config)
     {
         Config = config;
-        Server.PrintToConsole($"Found {Config.Sounds.Count} sounds!");
+
+        if (Config.Winner_Loser_Differentiation == true)
+        {
+            Server.PrintToConsole($"Found {Config.Sounds_Win.Count + Config.Sounds_Lose.Count} sounds!");
+        }
+        else
+        {
+            Server.PrintToConsole($"Found {Config.Sounds.Count} sounds!");
+        }
     }
 
     public override void Unload(bool hotReload)
@@ -34,17 +45,33 @@ public class EndRoundSoundsPlugin : BasePlugin, IPluginConfig<EndRoundSoundsConf
 
     private HookResult OnRoundEnd(EventRoundEnd @event, GameEventInfo info)
     {
-        if (Config.Sounds.Count == 0)
+        if ( (Config.Sounds_Win.Count == 0 && Config.Sounds_Lose.Count == 0 && Config.Winner_Loser_Differentiation == false) || (Config.Sounds.Count == 0 && Config.Winner_Loser_Differentiation == true) )
         {
             return HookResult.Continue;
         }
-        
-        var song_win = Config.Sounds_Win[Random.Shared.NextDistinct(Config.Sounds_Win.Count)];
-        var song_lose = Config.Sounds_Lose[Random.Shared.NextDistinct(Config.Sounds_Lose.Count)];
+
+
         var players = Utilities.GetPlayers();
         foreach (var player in players)
         {
-            player.ExecuteClientCommand($"play \"{song_win}\"");
+            if (Config.Winner_Loser_Differentiation == true)
+            {
+                if ((int)player.Team == @event.Winner)
+                {
+                    var song_win = Config.Sounds_Win[Random.Shared.NextDistinct(Config.Sounds_Win.Count)];
+                    player.ExecuteClientCommand($"play \"{song_win}\"");
+                }
+                else
+                {
+                    var song_lose = Config.Sounds_Lose[Random.Shared.NextDistinct(Config.Sounds_Lose.Count)];
+                    player.ExecuteClientCommand($"play \"{song_lose}\"");
+                }
+            }
+            else
+            {
+                var song = Config.Sounds[Random.Shared.NextDistinct(Config.Sounds.Count)];
+                player.ExecuteClientCommand($"play \"{song}\"");
+            }
         }
 
         return HookResult.Continue;
@@ -53,5 +80,8 @@ public class EndRoundSoundsPlugin : BasePlugin, IPluginConfig<EndRoundSoundsConf
 
 public class EndRoundSoundsConfig: BasePluginConfig
 {
+    public bool Winner_Loser_Differentiation { get; set; } = true;
+    public List<string> Sounds_Win { get; set; } = [];
+    public List<string> Sounds_Lose { get; set; } = [];
     public List<string> Sounds { get; set; } = [];
 }
