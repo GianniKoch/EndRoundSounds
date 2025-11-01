d832a41a77468caa494faf61a837afb971125b7c
Gianni Koch <me@giannikoch.com>
2025-11-01 10:30:23 +0100

    Refactored sound handling

--- EndRoundSounds/EndRoundSoundsPlugin.cs
+++ EndRoundSounds/EndRoundSoundsPlugin.cs
@@ -1,9 +1,7 @@
 ﻿using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes;
-using CounterStrikeSharp.API.Modules.Entities;
 using CounterStrikeSharp.API.Modules.Utils;
-using Microsoft.Extensions.Logging;
 
 namespace EndRoundSounds;
 
@@ -11,9 +9,11 @@ namespace EndRoundSounds;
 public class EndRoundSoundsPlugin : BasePlugin, IPluginConfig<EndRoundSoundsConfig>
 {
     public override string ModuleName => "End Round Sounds Plugin";
-    public override string ModuleVersion => "1.0.0";
+    public override string ModuleVersion => "1.1.0";
     public override string ModuleAuthor => "GianniKoch";
-    public override string ModuleDescription => "A plugin that plays configurable sounds at the end of each round.";
+
+    public override string ModuleDescription =>
+        "A basic plugin that plays configurable sounds at the end of each round.";
 
     public required EndRoundSoundsConfig Config { get; set; }
 
@@ -27,9 +27,10 @@ public class EndRoundSoundsPlugin : BasePlugin, IPluginConfig<EndRoundSoundsConf
     {
         Config = config;
 
-        if (Config.Winner_Loser_Differentiation == true)
+        if (Config.WinnerLoserDifferentiation)
         {
-            Server.PrintToConsole($"Found {Config.Sounds_Win.Count + Config.Sounds_Lose.Count} sounds!");
+            Server.PrintToConsole(
+                $"Using {nameof(Config.WinnerLoserDifferentiation)}, found {Config.SoundsWin.Count} win sounds and {Config.SoundsLose.Count} lose sounds!");
         }
         else
         {
@@ -43,45 +44,75 @@ public class EndRoundSoundsPlugin : BasePlugin, IPluginConfig<EndRoundSoundsConf
         Server.PrintToConsole("Unloaded End Round Sounds Plugin!");
     }
 
+    /// <summary>
+    /// Handle the round end event.
+    /// </summary>
+    /// <param name="event"></param>
+    /// <param name="info"></param>
+    /// <returns></returns>
     private HookResult OnRoundEnd(EventRoundEnd @event, GameEventInfo info)
     {
-        if ( (Config.Sounds_Win.Count == 0 && Config.Sounds_Lose.Count == 0 && Config.Winner_Loser_Differentiation == true) || (Config.Sounds.Count == 0 && Config.Winner_Loser_Differentiation == false) )
+        if (ShouldSkipEndRoundSounds())
         {
             return HookResult.Continue;
         }
 
-
         var players = Utilities.GetPlayers();
         foreach (var player in players)
         {
-            if (Config.Winner_Loser_Differentiation == true)
-            {
-                if ((int)player.Team == @event.Winner)
-                {
-                    var song_win = Config.Sounds_Win[Random.Shared.NextDistinct(Config.Sounds_Win.Count)];
-                    player.ExecuteClientCommand($"play \"{song_win}\"");
-                }
-                else
-                {
-                    var song_lose = Config.Sounds_Lose[Random.Shared.NextDistinct(Config.Sounds_Lose.Count)];
-                    player.ExecuteClientCommand($"play \"{song_lose}\"");
-                }
-            }
-            else
-            {
-                var song = Config.Sounds[Random.Shared.NextDistinct(Config.Sounds.Count)];
-                player.ExecuteClientCommand($"play \"{song}\"");
-            }
+            var sound = GetSoundPathForPlayer(player, (CsTeam)@event.Winner);
+            PlaySoundForPlayer(player, sound);
         }
 
         return HookResult.Continue;
     }
+
+    /// <summary>
+    /// Config is empty, or no sounds are set, skip playing the end sounds.
+    /// </summary>
+    /// <returns>Config is invalid</returns>
+    private bool ShouldSkipEndRoundSounds() =>
+        (Config.SoundsWin.Count == 0 && Config.SoundsLose.Count == 0 && Config.WinnerLoserDifferentiation) ||
+        (Config.Sounds.Count == 0 && !Config.WinnerLoserDifferentiation);
+
+    /// <summary>
+    /// Get a random sound path from the config.
+    /// </summary>
+    /// <param name="player">Player that sounds needs to play for</param>
+    /// <param name="winningTeam">Winning team of the round</param>
+    private string GetSoundPathForPlayer(CCSPlayerController player, CsTeam winningTeam)
+    {
+        if (Config.WinnerLoserDifferentiation)
+        {
+            if (player.Team == winningTeam)
+            {
+                return Config.SoundsWin[Random.Shared.NextDistinct(Config.SoundsWin.Count)];
+            }
+            else
+            {
+                return Config.SoundsLose[Random.Shared.NextDistinct(Config.SoundsLose.Count)];
+            }
+        }
+        else
+        {
+            return Config.Sounds[Random.Shared.NextDistinct(Config.Sounds.Count)];
+        }
+    }
+
+    /// <summary>
+    /// Send a client command to play a sound from a workshop item for the given player.
+    /// Check the README.md of this project for more info about creating the necessary workshop item.
+    /// </summary>
+    /// <param name="player"></param>
+    /// <param name="path">Path to sound file in workshop items</param>
+    private static void PlaySoundForPlayer(CCSPlayerController player, string path) =>
+        player.ExecuteClientCommand($"play \"{path}\"");
 }
 
-public class EndRoundSoundsConfig: BasePluginConfig
+public class EndRoundSoundsConfig : BasePluginConfig
 {
-    public bool Winner_Loser_Differentiation { get; set; } = true;
-    public List<string> Sounds_Win { get; set; } = [];
-    public List<string> Sounds_Lose { get; set; } = [];
+    public bool WinnerLoserDifferentiation { get; set; } = true;
+    public List<string> SoundsWin { get; set; } = [];
+    public List<string> SoundsLose { get; set; } = [];
     public List<string> Sounds { get; set; } = [];
 }
